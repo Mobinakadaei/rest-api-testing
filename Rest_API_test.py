5ff0ae9f6d17f2c7004231d81da2bfb4485bbb99
alimostafaei <ZxcvbnM@987654321>
2024-10-20 22:14:58 +0200

    adding rest-api-testing files

--- Rest_API_test.py
+++ Rest_API_test.py
@@ -1,8 +1,8 @@
 from requests import get
+from Rest_API_test_variable import *
 
 
-def get_animal_facts(animal_type="cat", amount="2"):
-    end_point = "https://cat-fact.herokuapp.com/facts/random"
+def get_animal_facts(animal_type=valid_animals[0], amount=random_test_amount):
     params = {}
     if animal_type:
         params['animal_type'] = animal_type
@@ -12,53 +12,51 @@ def get_animal_facts(animal_type="cat", amount="2"):
     return response
 
 
-def test_animal_facts_status_code(animal_type="cat", amount="2"):
+def test_animal_facts_status_code(animal_type=valid_animals[0], amount=random_test_amount):
     status_code = get_animal_facts(animal_type, amount).status_code
     assert status_code == 200
 
 
 def test_animal_type_is_none(animal_type=None):
+    """
+        Test the response when animal_type parameter is not included
+    """
     response = get_animal_facts(animal_type=animal_type).json()
     for item in response:
         assert item['type'] == "cat", f"Expected type '{animal_type}', but got '{item['type']}'"
 
 
 def test_animal_type_is_empty(animal_type=""):
+    """
+        Test the response when animal_type parameter is set to null
+    """
     response = get_animal_facts(animal_type=animal_type).json()
     for item in response:
         assert item['type'] == "cat", f"Expected type '{animal_type}', but got '{item['type']}'"
 
 
 def test_animal_type_is_not_in_the_list(animal_type="lion"):
+    """
+        Test the response when animal_type parameter is not set to a valid animal name.
+    """
     response = get_animal_facts(animal_type=animal_type).json()
     assert response == []
 
 
-def test_animal_type_is_cat(animal_type="cat"):
-    response = get_animal_facts(animal_type=animal_type).json()
-    for item in response:
-        assert item['type'] == animal_type, f"Expected type '{animal_type}', but got '{item['type']}'"
-
-
-def test_animal_type_is_dog(animal_type="dog"):
-    response = get_animal_facts(animal_type=animal_type).json()
-    for item in response:
-        assert item['type'] == animal_type, f"Expected type '{animal_type}', but got '{item['type']}'"
-
-
-def test_animal_type_is_snail(animal_type="snail"):
-    response = get_animal_facts(animal_type=animal_type).json()
-    for item in response:
-        assert item['type'] == animal_type, f"Expected type '{animal_type}', but got '{item['type']}'"
-
-
-def test_animal_type_is_horse(animal_type="horse"):
-    response = get_animal_facts(animal_type=animal_type).json()
-    for item in response:
-        assert item['type'] == animal_type, f"Expected type '{animal_type}', but got '{item['type']}'"
+def test_animal_type_is_in_valid_list():
+    """
+        Test the response when animal_type parameter is set to valid animals names.
+    """
+    for animal_type in valid_animals:
+        response = get_animal_facts(animal_type=animal_type).json()
+        for item in response:
+            assert item['type'] == animal_type, f"Expected type '{animal_type}', but got '{item['type']}'"
 
 
 def test_animal_type_and_amount_field_is_null(animal_type=None, amount=None):
+    """
+        Test the response when amount parameter and animal_type parameter is set to null
+    """
     response = get_animal_facts(animal_type=animal_type, amount=amount).json()
     assert response["type"] == "cat"
     if isinstance(response, dict):
@@ -67,6 +65,9 @@ def test_animal_type_and_amount_field_is_null(animal_type=None, amount=None):
 
 
 def test_amount_field_is_none(amount=None):
+    """
+        Test the response when amount parameter is not included
+    """
     response = get_animal_facts(amount=amount).json()
     if isinstance(response, dict):
         response = [response]
@@ -74,11 +75,17 @@ def test_amount_field_is_none(amount=None):
 
 
 def test_amount_field_is_zero(amount="0"):
+    """
+        Test the response when amount parameter is set to zero
+    """
     response = get_animal_facts(amount=amount).json()
     assert response == []
 
 
 def test_amount_field_is_empty(amount=""):
+    """
+        Test the response when amount parameter is set to null
+    """
     response = get_animal_facts(amount=amount).json()
     if isinstance(response, dict):
         response = [response]
@@ -86,16 +93,25 @@ def test_amount_field_is_empty(amount=""):
 
 
 def test_amount_field_has_normal_value(amount="5"):
+    """
+        Test the response when amount parameter is set to a random normal value
+    """
     response = get_animal_facts(amount=amount).json()
     assert str(len(response)) == amount
 
 
-def test_amount_field_has_max_value(amount="500"):
+def test_amount_field_has_max_value(amount=maximum_value):
+    """
+        Test the response when amount parameter is set to maximum valid value
+    """
     response = get_animal_facts(amount=amount).json()
     assert str(len(response)) == amount
 
 
-def test_amount_field_has_more_than_max_value(amount="504"):
+def test_amount_field_has_more_than_max_value(amount=bigger_than_maximum_value):
+    """
+        Test the response when amount parameter is set to a number bigger than valid value
+    """
     response = get_animal_facts(amount=amount).json()
     assert response["message"] == "Limited to 500 facts at a time"
 
---
+++ Rest_API_test_variable.py
@@ -0,0 +1,5 @@
+end_point = "https://cat-fact.herokuapp.com/facts/random"
+valid_animals = ["cat", "dog", "horse", "snail"]
+random_test_amount = "2"
+maximum_value = "500"
+bigger_than_maximum_value = "504"
